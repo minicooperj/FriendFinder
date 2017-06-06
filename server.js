625d636c647dfa0d218d1e772298d3e76990a951
Cary Cooper <Cary Cooper>
2017-06-06 02:20:59 -0400

    p

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
